043dae3d947b1aba28e2f764c59b453bde35978d
Caroline-CD <caroline.copy19@gmail.com>
2022-05-31 23:51:17 +0200

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
      <Logo />
           <div class="flex flex-col items-center pt-[100px]"> 
                <button class="bg-white text-purple-element tracking-wide h-16 p-4 font-bold uppercase text-2xl rounded-full">Je commande</button>
-               <button class="bg-white text-purple-element tracking-wide h-16 p-4 font-bold uppercase text-2xl rounded-full">Qui sommes-nous ?</button>
+               <button class="bg-white text-purple-element tracking-wide h-16 p-4 mt-4 font-bold uppercase text-2xl rounded-full">Qui sommes-nous ?</button>
 
           </div>
 </div>
