4ace8f509f45ac0a2f4cd251c9d6b69c8a3ad5c9
Caroline-CD <caroline.copy19@gmail.com>
2022-06-02 11:29:33 +0200

    wip

--- .nuxt/types/middleware.d.ts
+++
@@ -1,7 +0,0 @@
-import type { NavigationGuard } from 'vue-router'
-export type MiddlewareKey = string
-declare module "/home/caroline/Bureau/Mon-Kit-Apero/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
-  }
-}
